94453ba07cb1a75f36d70d0faea5d81906de679a
andiputraw <andiputraw925@gmail.com>
2023-06-15 11:11:36 +0700

    fix env issue

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
-import { load } from "https://deno.land/std@0.188.0/dotenv/mod.ts";
-
-const env = await load();
-
 const CONFIG = {
-  SUPABASE_URL: env["SUPABASE_URL"] ?? "",
-  SUPABASE_KEY: env["SUPABASE_KEY"] ?? "",
+  SUPABASE_URL: Deno.env.get("SUPABASE_URL") ?? "",
+  SUPABASE_KEY: Deno.env.get("SUPABASE_KEY") ?? "",
 };
 
 export default CONFIG;
--- dev.ts
+++ dev.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
 import dev from "$fresh/dev.ts";
+import "https://deno.land/std@0.188.0/dotenv/load.ts";
 
 await dev(import.meta.url, "./main.ts");
